19bf28d5a1d4a03803a2964daf6ad5087bd9725a
amine <amine@LAPTOP-4UOIPTCD>
2020-03-08 12:11:24 +0100

    port ennemi

--- Projet GLP/Amine/Port/PortEnnemi.java
+++ Projet GLP/Amine/Port/PortEnnemi.java
@@ -1,10 +1,14 @@
 package Port;
 
+import DonnéesPort.Flotte;
+import Navire.NavireAttaque;
+import Navire.NavireTransport;
+
 public class PortEnnemi extends Port{
 
-	public PortEnnemi(String name,int valeurDeLaVille, int gold, int wood, int steel, int food, int level)
+	public PortEnnemi(String name,int valeurDeLaVille, int gold, int wood, int steel, int food, int level,Flotte flotte,NavireAttaque navireAttaque, NavireTransport navireTransport)
 	{
-		super(name,valeurDeLaVille, gold, wood, steel, food, level);
+		super(name,valeurDeLaVille, gold, wood, steel, food, level,flotte,navireAttaque, navireTransport);
 	}
 	
 	
