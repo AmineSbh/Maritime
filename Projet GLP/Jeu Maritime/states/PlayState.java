c72a58cb75f12510ddbf14b6e11deb1f68cc5138
Qxillum <Qxillum@gmail.com>
2020-03-24 14:00:53 +0100

    Modification souris

--- Projet GLP/Jeu Maritime/states/PlayState.java
+++ Projet GLP/Jeu Maritime/states/PlayState.java
@@ -1,12 +1,15 @@
 package states;
 
 import java.awt.Graphics2D;
+
 import utils.MouseHandler;
 import tiles.TileManager;
 
 public class PlayState extends GameState {
 	
+	private int mousefirst = 0;
 	private TileManager tm;
+	
 	
 	public PlayState(GameStateManager gsm) {
 		super(gsm);
@@ -17,6 +20,11 @@ public class PlayState extends GameState {
 	public void update() {
 	}
 	public void input(MouseHandler mouse) {
+		if ((mouse.getButton() == 1) && (mousefirst  == 0)) {
+			mousefirst += 1;
+			System.out.println("Vous avez cliquer sur un port");
+		}
+            
 	}
 	public void render(Graphics2D g) {
 		tm.render(g);
