46986b1e2fadc7f4292ee2254c2ce5cbf8fb3b23
amine <amine@LAPTOP-4UOIPTCD.home>
2020-04-17 17:45:16 +0200

    lvlup

--- Projet GLP/Jeu Maritime/GuiPort/TestCarte.java
+++ Projet GLP/Jeu Maritime/GuiPort/TestCarte.java
@@ -38,26 +38,7 @@ public class TestCarte extends JFrame {
 	private JPanel contentPane;
 	
 	private Moteur moteur=new Moteur();
-
-	/**
-	 * Launch the application.
-	 */
-	/*public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					TestCarte frame = new TestCarte();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}*/
-
-	/**
-	 * Create the frame.
-	 */
+	
 	public TestCarte() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
